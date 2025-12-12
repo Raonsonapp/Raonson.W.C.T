851d724a118b8e672b4eff40ccca305a202480ef
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-13 03:23:13 +0500

    Update post_model.dart

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,7 +1,39 @@
 class PostModel {
   final String id;
-  final String image;
+  final String userId;
+  final String imageUrl;
   final String caption;
+  final int likes;
+  final DateTime createdAt;
 
-  PostModel({required this.id, required this.image, required this.caption});
+  PostModel({
+    required this.id,
+    required this.userId,
+    required this.imageUrl,
+    required this.caption,
+    required this.likes,
+    required this.createdAt,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'] ?? '',
+      userId: json['userId'] ?? '',
+      imageUrl: json['imageUrl'] ?? '',
+      caption: json['caption'] ?? '',
+      likes: json['likes'] ?? 0,
+      createdAt: (json['createdAt'] != null)
+          ? DateTime.parse(json['createdAt'])
+          : DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "userId": userId,
+        "imageUrl": imageUrl,
+        "caption": caption,
+        "likes": likes,
+        "createdAt": createdAt.toIso8601String(),
+      };
 }
