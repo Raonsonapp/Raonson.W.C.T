d9320b56346b1023a4edabe05528128fefb5d9df
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-13 21:31:41 +0500

    Create kingai_screen.dart

---
+++ lib/screens/kingai_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class KingAIScreen extends StatelessWidget {
+  const KingAIScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("KING AI Builder")),
+    );
+  }
+}
